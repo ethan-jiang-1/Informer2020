d1221a731d8a61237114e3b0fb1135cf6fcd35a7
ethanmac <ethanmac@mac.com>
2021-04-21 19:39:51 +0800

    update

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -423,3 +423,41 @@ class Exp_Informer(Exp_Basic):
         np.save(folder_path+'real_prediction.npy', preds)
         
         return
+
+    def predict_raw_item(self, setting, batch_x,batch_y,batch_x_mark,batch_y_mark, load=False):
+        if load:
+            path = os.path.join(self.args.checkpoints, setting)
+            best_model_path = path+'/'+'checkpoint.pth'
+            if os.path.isfile(best_model_path):
+                self._load_saved_state(best_model_path)
+
+        self.model.eval()
+        
+        batch_x = batch_x.float().to(self.device)
+        batch_y = batch_y.float()
+        batch_x_mark = batch_x_mark.float().to(self.device)
+        batch_y_mark = batch_y_mark.float().to(self.device)
+
+        # decoder input
+        dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[-1]]).float()
+        dec_inp = torch.cat([batch_y[:,:self.args.label_len,:], dec_inp], dim=1).float().to(self.device)
+        # encoder - decoder
+        if self.args.use_amp:
+            with torch.cuda.amp.autocast():
+                if self.args.output_attention:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                else:
+                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+        else:
+            if self.args.output_attention:
+                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+            else:
+                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+        #if self.args.inverse:
+        #    outputs = pred_data.inverse_transform(outputs)
+        f_dim = -1 if self.args.features=='MS' else 0
+        batch_y = batch_y[:,-self.args.pred_len:,f_dim:].to(self.device)
+        
+        pred = outputs.detach().cpu().numpy()#.squeeze()
+        
+        return pred
